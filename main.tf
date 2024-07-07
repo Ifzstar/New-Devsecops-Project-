233a4e2b2a14526e09fb82cf4ab7e3dc130968b0
“name” <you@example.com>
2024-07-07 14:50:43 -0400

    commit

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ provider "aws" {
 resource "aws_s3_bucket" "terraform-march-ife" {
   bucket = "terraform-march-ife"
 }
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-ife"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
